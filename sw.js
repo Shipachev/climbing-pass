8e86b1b806f409e6ad91a3c3147568d99ddaad9f
Maxim Shipachev <maxim@shipachev.ru>
2026-03-13 22:56:36 +0700

    Обновление версии

--- sw.js
+++ sw.js
@@ -1,13 +1,66 @@
-const CACHE = 'climbpass-v1';
+const CACHE_VERSION = 'climbpass-v1';
 
-const ASSETS = ['./', './index.html', './manifest.json'];
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+    './sw.js',
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+// INSTALL
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_VERSION).then((cache) => {
+            return cache.addAll(APP_SHELL);
+        }),
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request)),
+// ACTIVATE
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_VERSION) {
+                        return caches.delete(key);
+                    }
+                }),
+            );
+        }),
     );
+
+    return self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            if (cached) return cached;
+
+            return fetch(event.request).then((response) => {
+                let clone = response.clone();
+
+                caches.open(CACHE_VERSION).then((cache) => {
+                    cache.put(event.request, clone);
+                });
+
+                return response;
+            });
+        }),
+    );
+});
+
+// FORCE UPDATE FROM PAGE
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
